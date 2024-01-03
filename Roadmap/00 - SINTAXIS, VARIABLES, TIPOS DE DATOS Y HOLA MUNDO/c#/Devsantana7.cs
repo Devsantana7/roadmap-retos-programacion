47e604fd9483faa23b0435d7c86f2e68745d0957
santanac7 <148362420+Devsantana7@users.noreply.github.com>
2024-01-03 12:28:52 -0500

    #00-C#

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/Devsantana7.cs
@@ -0,0 +1,20 @@
+// https://learn.microsoft.com/en-us/dotnet/csharp/
+
+/*
+This is an example for
+multi-line comments in 
+C# language programming
+*/
+
+using System.Net.NetworkInformation;
+
+int num = 1;
+const double Pi = 3.1415;
+
+string name = "Carlos";
+char singleChar = 'a';
+int age = 31;
+double decimalNum = 34.14;
+bool isAlive = true;
+
+Console.WriteLine("¡Hola, C#!");
