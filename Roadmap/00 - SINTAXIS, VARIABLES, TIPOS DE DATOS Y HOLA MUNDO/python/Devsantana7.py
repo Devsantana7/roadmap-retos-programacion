8b567573a55f7056538d485785c98d25e4957e1c
santanac7 <148362420+Devsantana7@users.noreply.github.com>
2024-01-04 07:25:07 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Devsantana7.py
@@ -0,0 +1,25 @@
+# https://www.python.org/
+
+# Este es un comentario de una sola linea
+
+"""
+Este es un comentario
+en multiples lineas
+"""
+
+'''
+Esta es otra forma de
+realizar comentarios en 
+varias lineas
+'''
+
+name = "Carlos" 
+NAME_COUNTRY = "Colombia" # Python no tine constantes, así que una forma de hacerlo es por convención. Declarando la variable en mayúsculas
+
+# Python es un lenguaje debilmente tipado, por lo que los valores de una variable pueden mutar a cualquier tipo de dato
+string = "Python" # Dato de tipo string
+age = 31 # Dato tipo entero
+PI = 3.1415 # Dato tipo decimal
+isAlive = True # Dato tipo booleano
+
+print("¡Hola, Python!")
